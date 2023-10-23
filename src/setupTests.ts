7480057afeef1279458f71e43162798ca1ed2c84
James Rucker <james@rucker.land>
2023-10-23 12:11:44 -0700

    Fix dom-jest vitest setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,2 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
-
-expect.extend(matchers);
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@testing-library/jest-dom/vitest';
