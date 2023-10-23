8074bc7650d7e0f916a3a17ad10e96c95912e60e
James Rucker <james@rucker.land>
2023-10-22 22:26:18 -0700

    Add Vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
